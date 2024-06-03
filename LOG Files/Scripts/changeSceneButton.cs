6cccba507d9ecd68a72988a35b398746b19d8b7d
AutisticRobot <tmkormann04@gmail.com>
2024-06-02 19:21:31 -0700

    created main menu

---
+++ LOG Files/Scripts/changeSceneButton.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class changeSceneButton : Button
+{
+	[Export] public PackedScene changeToScene;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void onButtonPress()
+	{
+		GetTree().ChangeSceneToPacked(changeToScene);
+	}
+}
