8639178fc24a2759838e113c61ac2ef20c99c1b7
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2024-06-10 22:42:41 -0700

    created panel.cs

---
+++ LOG Files/Scripts/VisualMode/Panel.cs
@@ -0,0 +1,82 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Panel : Control
+{
+	[Export] VisualMode SceneMan;
+			 public int XSize;
+			 public int YSize;
+			 public Vector2 BoardLength;
+			 public Vector2 BoardPos;
+	[Export] public PackedScene SwitchPrefab;
+				public Vector2 SwitchScale;
+			[Export] public float SwitchScaleModifier;
+			 public List<Switch> allSwitches = new();
+			 public Grid State;
+	// Called when the node enters the scene tree for the first time.
+	public void Start()
+	{
+		XSize = SceneMan.XSize;
+		YSize = SceneMan.YSize;
+		BoardLength = SceneMan.BoardLength;
+		BoardPos = SceneMan.SwitchBoard.Position;
+
+		SwitchScale = new
+		(
+			(BoardLength.X * (1 + SwitchScaleModifier)) / XSize ,
+			(BoardLength.X * (1 + SwitchScaleModifier)) / XSize  
+		);
+		for(int Y=0;Y<YSize;Y++)
+		{
+			for(int X=0;X<XSize;X++)
+			{
+				CreateSwitch(X,Y);
+			}
+
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public void UpdateState(Grid grid)
+	{
+		State = grid;
+	}
+	
+	public void CreateSwitch(int X, int Y)
+	{
+		//GD.Print("X: " + X + " Y: " + Y);
+
+		Switch swt = SwitchPrefab.Instantiate<Switch>();
+		float outX;
+		float outY;
+
+
+		//Position relitive to top right of Board
+		outX = ((float)BoardLength.X/XSize) * X;
+		outY = ((float)BoardLength.Y/YSize) * Y;
+
+		//off set to account for being relative to center of switch
+		outX += ((float)BoardLength.X)/(XSize * 2);
+		outY += ((float)BoardLength.Y)/(YSize * 2);
+
+		//off set to account for being relative to center of Board
+		outX -= BoardLength.X/2;
+		outY -= BoardLength.Y/2;
+
+		//offset to make relative to global
+		outX += BoardPos.X;
+		outY += BoardPos.Y;
+
+        swt.Position = new Vector2(outX,outY);
+		swt.Scale = SwitchScale / swt.Texture.GetSize();
+		swt.X = X;
+		swt.Y = Y;
+
+
+		//GD.Print(swt.Position);
+		AddChild(swt);
+		allSwitches.Add(swt);
+
+	}
+}
--- LOG Files/Scripts/VisualMode/VisualMode.cs
+++ LOG Files/Scripts/VisualMode/VisualMode.cs
@@ -10,31 +10,23 @@ public partial class VisualMode : Node2D
 			 public Vector2 BoardLength;
 	[Export] public MaskedGrid GridState;
 	[Export] public Grid solution;
-	[Export] public PackedScene SwitchPrefab;
-				public Vector2 SwitchScale;
-			[Export] public float SwitchScaleModifier;
-			 public List<Switch> allSwitches = new();
+
+	[Export] public Panel LightBoard;
+	[Export] public Panel FuseBoard;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		BoardLength = SwitchBoard.Scale * SwitchBoard.Texture.GetSize();
-		SwitchScale = new
-		(
-			(BoardLength.X * (1 + SwitchScaleModifier)) / XSize ,
-			(BoardLength.X * (1 + SwitchScaleModifier)) / XSize  
-		);
 
 		solution.startGrid(XSize, YSize);
 		GD.Print("Solution is set = " + Grid.isReady(solution));
 
-		for(int Y=0;Y<YSize;Y++)
-		{
-			for(int X=0;X<XSize;X++)
-			{
-				CreateSwitch(X,Y);
-			}
+		LightBoard.UpdateState(GridState);
+		FuseBoard.UpdateState(solution);
 
-		}
+
+		LightBoard.Start();
+		FuseBoard.Start();
 	
 	}
 
@@ -43,40 +35,4 @@ public partial class VisualMode : Node2D
 	{
 	}
 	
-	public void CreateSwitch(int X, int Y)
-	{
-		//GD.Print("X: " + X + " Y: " + Y);
-
-		Switch swt = SwitchPrefab.Instantiate<Switch>();
-		float outX;
-		float outY;
-
-
-		//Position relitive to top right of Board
-		outX = ((float)BoardLength.X/XSize) * X;
-		outY = ((float)BoardLength.Y/YSize) * Y;
-
-		//off set to account for being relative to center of switch
-		outX += ((float)BoardLength.X)/(XSize * 2);
-		outY += ((float)BoardLength.Y)/(YSize * 2);
-
-		//off set to account for being relative to center of Board
-		outX -= BoardLength.X/2;
-		outY -= BoardLength.Y/2;
-
-		//offset to make relative to global
-		outX += SwitchBoard.Position.X;
-		outY += SwitchBoard.Position.Y;
-
-        swt.Position = new Vector2(outX,outY);
-		swt.Scale = SwitchScale / swt.Texture.GetSize();
-		swt.X = X;
-		swt.Y = Y;
-
-
-		//GD.Print(swt.Position);
-		AddChild(swt);
-		allSwitches.Add(swt);
-
-	}
 }
